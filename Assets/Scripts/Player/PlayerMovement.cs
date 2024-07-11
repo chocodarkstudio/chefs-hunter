2265d54ee760f291df4c47b571909e31e64cfe17
Felipe Gutierrez Romero <felipegutiromero@gmail.com>
2024-07-11 18:14:49 -0500

    Script del movimiento creado

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,5 +2,23 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float moveSpeed = 3.5f;
+    private Rigidbody rb;
+    private Vector2 movement;
 
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+       rb.MovePosition(rb.position + (Vector3)movement * Time.fixedDeltaTime * moveSpeed);
+    }
 }
