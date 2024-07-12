282f8bbf10f643948d00e1f864692ccb2a733e61
chocodark <124619957+chocodarkstudio@users.noreply.github.com>
2024-07-12 17:02:44 -0300

    fixed enemies and weapon config

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -5,4 +5,6 @@ public class Enemy : MonoBehaviour
 {
     [field: SerializeField] public ItemIngredientObj Type { get; protected set; }
     [field: SerializeField] public ItemIngredientObj[] Drops { get; protected set; }
+
+    [field: SerializeField] public ItemWeaponObj[] WeaknessesWeapons { get; protected set; }
 }
--- Assets/Scripts/Items/ItemWeapon.cs
+++ Assets/Scripts/Items/ItemWeapon.cs
@@ -1,12 +1,9 @@
-using UnityEngine;
-
 namespace Items
 {
 
     [System.Serializable]
     public class ItemWeapon : Item
     {
-        [field: SerializeField] public ItemIngredientObj[] Attack { get; protected set; }
-        [field: SerializeField] public ItemIngredientObj[] Defense { get; protected set; }
+
     }
 }
